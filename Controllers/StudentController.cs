4af03cc4604d5ce288403dee9f38ecd8bd894693
Vignesh12345-hub <vignesh@gsstech.in>
2026-04-09 12:34:52 +0530

    first commit

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public string GetStudent()
+        {
+            return "Hello Student API";
+        }
+    }
+}
